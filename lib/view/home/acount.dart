413faa7cf1933f6bfd489d0130808bcbb5c11c44
Your Name <you@example.com>
2024-03-14 01:30:38 +0300

    update change

--- lib/view/home/acount.dart
+++ lib/view/home/acount.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get_connect/http/src/utils/utils.dart';
 
 class Account extends StatelessWidget {
   @override
@@ -8,10 +9,42 @@ class Account extends StatelessWidget {
       children: [
         Container(
           height: 300.h,
-          decoration: BoxDecoration(
+          decoration: const BoxDecoration(
             color: Color(0xFFD9D9D9),
-            borderRadius:
-                BorderRadius.only(bottomRight: Radius.circular(150.0)),
+            borderRadius: BorderRadius.only(
+              bottomRight: Radius.circular(100.0),
+              bottomLeft: Radius.circular(100.0),
+            ),
+          ),
+          child: const Column(
+            children: [
+              Center(
+                child: Text(
+                  "حسابك",
+                  style: TextStyle(
+                    fontFamily: 'Cairo',
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.w700,
+                    height: 1.5, // line height
+                    letterSpacing: 0.0,
+                    color: Color.fromRGBO(2, 22, 34, 1),
+                  ),
+                  textAlign: TextAlign.right,
+                ),
+              ),
+              Text(
+                "الاسم",
+                style: TextStyle(
+                  fontFamily: 'Cairo',
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w700,
+                  height: 1.5, // line height
+                  letterSpacing: 0.0,
+                  color: Color.fromRGBO(2, 22, 34, 1),
+                ),
+                textAlign: TextAlign.right,
+              ),
+            ],
           ),
         ),
       ],
---
+++ lib/view/login/login_page
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class Account extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 300.h,
+          decoration: BoxDecoration(
+            color: Color(0xFFD9D9D9),
+            borderRadius:
+                BorderRadius.only(bottomRight: Radius.circular(150.0)),
+          ),
+        ),
+      ],
+    );
+  }
+}
